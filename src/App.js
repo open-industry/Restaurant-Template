bfc880ba84aeb9a188b78b35e0f4b3051084b052
anon-legion <g.sync.mobile1@gmail.com>
2021-11-23 21:53:50 +0800

    add working routes and links

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import Home from './components/home.js';
+import Menu from './components/menu.js';
+import Contact from './components/contact.js';
 import './App.css';
 import 'bulma/css/bulma.min.css';
 
 function App() {
 	return (		
-		<>
+		<Router>
 			<header className="hero has-text-centered has-background-warning">
 				<h1 className="title is-1 has-text-danger-dark">McDnld's Rstrnt</h1>
-				<h2 className="subtitle is-3">Fine Slow Food Cuisine</h2>				
+				<h2 className="subtitle is-3">Fine Dilatory Cuisine</h2>				
 			</header>
 			<main className="section has-background-danger-dark">
-				<div className="container has-background-danger-light">
+				<div className="container has-background-white-ter">
 					<div className="tabs is-large is-boxed is-centered">
 						<ul>
-							<li><a className="has-text-warning-dark">Home</a></li>
-							<li><a className="has-text-warning-dark">Menu</a></li>
-							<li><a className="has-text-warning-dark">Contact</a></li>
+							<li><Link to="/" className="has-text-warning-dark">Home</Link></li>
+							<li><Link to="/menu" className="has-text-warning-dark">Menu</Link></li>
+							<li><Link to="/contact" className="has-text-warning-dark">Contact</Link></li>
 						</ul>
 					</div>
 				</div>
-				<div className="section has-background-danger-light">
+				<div className="section has-background-white-ter">
+					<Routes>
+						<Route path="/" element={<Home/>} />
+						<Route path="/menu" element={<Menu/>} />
+						<Route path="/contact" element={<Contact/>} />
+					</Routes>
 				</div>
 			</main>
-		</>
+		</Router>
 	);
 }
 
