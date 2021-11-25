e07d947c15ca62102bffdf05611d27a4623add63
anon-legion <g.sync.mobile1@gmail.com>
2021-11-25 13:11:16 +0800

    update router and nav link state

--- src/App.js
+++ src/App.js
@@ -1,37 +1,73 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import React, { useState } from 'react';
+// BrowserRouter in index.js
+import { Route, Routes, Link } from 'react-router-dom';
 import Home from './components/home.js';
 import Menu from './components/menu.js';
 import Contact from './components/contact.js';
 import './App.css';
 import 'bulma/css/bulma.min.css';
 
+const navItems = ['Home', 'Menu', 'Contact'];
+
+// icon className selector function for font awesome
+const iconClass = (nav) => {
+	switch (nav) {
+			case navItems[0]:
+				return 'fas fa-house';
+			case navItems[1]:
+				return 'fas fa-utensils';
+			case navItems[2]:
+				return 'fas fa-phone';
+			default:
+				return 'fas fa-house';
+	};
+};
+
 function App() {
+	const [activeNav, setActiveNav] = useState(() => navItems[0]);
+
+	const handleNavClick = (nav) => {
+		setActiveNav(pevNav => nav);
+	}
+
 	return (		
-		<Router>
-			<header className="hero has-text-centered has-background-warning">
-				<h1 className="title is-1 has-text-danger-dark">McDnld's Rstrnt</h1>
-				<h2 className="subtitle is-3">Fine Dilatory Cuisine</h2>				
+		<>
+			<header className="hero has-text-centered has-background-danger-dark">
+				<h1 className="title is-1 has-text-warning">McDnld's Rstrnt</h1>
+				<h2 className="subtitle is-3 has-text-white-ter">Fine Dilatory Cuisine</h2>				
 			</header>
 			<main className="section has-background-danger-dark">
-				<div className="container has-background-white-ter">
-					<div className="tabs is-large is-boxed is-centered">
-						<ul>
-							<li><Link to="/" className="has-text-warning-dark">Home</Link></li>
-							<li><Link to="/menu" className="has-text-warning-dark">Menu</Link></li>
-							<li><Link to="/contact" className="has-text-warning-dark">Contact</Link></li>
-						</ul>
+				<div className="box">
+					<div className="container has-background-white">
+						<div className="tabs is-large is-boxed is-centered">
+							<ul>
+								{navItems.map((item) => (
+									<li key={item} className={item === activeNav ? "is-active" : null}>
+										<Link
+											to={`/${item !== navItems[0] ? item.toLowerCase() : ""}`}
+											className={item === activeNav ? "active-font" : "has-text-warning-dark"}
+											onClick={() => handleNavClick(item)}
+										>
+											<span className="icon is-small has-text-grey-dark">
+												<i className={`${iconClass(item)} is-size-6`}></i>
+											</span>
+											{item}
+										</Link>
+									</li>
+								))}							
+							</ul>
+						</div>
+					</div>
+					<div className="container has-background-white">
+						<Routes>
+							<Route path="/menu" element={<Menu/>} />
+							<Route path="/contact" element={<Contact/>} />
+							<Route path="/" element={<Home/>} />
+						</Routes>
 					</div>
 				</div>
-				<div className="section has-background-white-ter">
-					<Routes>
-						<Route path="/" element={<Home/>} />
-						<Route path="/menu" element={<Menu/>} />
-						<Route path="/contact" element={<Contact/>} />
-					</Routes>
-				</div>
 			</main>
-		</Router>
+		</>
 	);
 }
 
