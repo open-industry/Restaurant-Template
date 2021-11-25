18e5280823ad871e93e30e84f0d64a0f3d922bd9
anon-legion <g.sync.mobile1@gmail.com>
2021-11-25 13:52:23 +0800

    update app source code formatting

--- src/App.js
+++ src/App.js
@@ -11,64 +11,64 @@ const navItems = ['Home', 'Menu', 'Contact'];
 
 // icon className selector function for font awesome
 const iconClass = (nav) => {
-	switch (nav) {
-			case navItems[0]:
-				return 'fas fa-house';
-			case navItems[1]:
-				return 'fas fa-utensils';
-			case navItems[2]:
-				return 'fas fa-phone';
-			default:
-				return 'fas fa-house';
-	};
+  switch (nav) {
+      case navItems[0]:
+        return 'fas fa-house';
+      case navItems[1]:
+        return 'fas fa-utensils';
+      case navItems[2]:
+        return 'fas fa-phone';
+      default:
+        return 'fas fa-house';
+  };
 };
 
 function App() {
-	const [activeNav, setActiveNav] = useState(() => navItems[0]);
+  const [activeNav, setActiveNav] = useState(() => navItems[0]);
 
-	const handleNavClick = (nav) => {
-		setActiveNav(pevNav => nav);
-	}
+  const handleNavClick = (nav) => {
+    setActiveNav(pevNav => nav);
+  }
 
-	return (		
-		<>
-			<header className="hero has-text-centered has-background-danger-dark">
-				<h1 className="title is-1 has-text-warning">McDnld's Rstrnt</h1>
-				<h2 className="subtitle is-3 has-text-white-ter">Fine Dilatory Cuisine</h2>				
-			</header>
-			<main className="section has-background-danger-dark">
-				<div className="box">
-					<div className="container has-background-white">
-						<div className="tabs is-large is-boxed is-centered">
-							<ul>
-								{navItems.map((item) => (
-									<li key={item} className={item === activeNav ? "is-active" : null}>
-										<Link
-											to={`/${item !== navItems[0] ? item.toLowerCase() : ""}`}
-											className={item === activeNav ? "active-font" : "has-text-warning-dark"}
-											onClick={() => handleNavClick(item)}
-										>
-											<span className="icon is-small has-text-grey-dark">
-												<i className={`${iconClass(item)} is-size-6`}></i>
-											</span>
-											{item}
-										</Link>
-									</li>
-								))}							
-							</ul>
-						</div>
-					</div>
-					<div className="container has-background-white">
-						<Routes>
-							<Route path="/menu" element={<Menu/>} />
-							<Route path="/contact" element={<Contact/>} />
-							<Route path="/" element={<Home/>} />
-						</Routes>
-					</div>
-				</div>
-			</main>
-		</>
-	);
+  return (		
+    <>
+      <header className="hero has-text-centered has-background-danger-dark">
+        <h1 className="title is-1 has-text-warning">McDnld's Rstrnt</h1>
+        <h2 className="subtitle is-3 has-text-white-ter">Fine Dilatory Cuisine</h2>				
+      </header>
+      <main className="section has-background-danger-dark">
+        <div className="box">
+          <div className="container has-background-white">
+            <div className="tabs is-large is-boxed is-centered">
+              <ul>
+                {navItems.map((item) => (
+                  <li key={item} className={item === activeNav ? "is-active" : null}>
+                    <Link
+                      to={`/${item !== navItems[0] ? item.toLowerCase() : ""}`}
+                      className={item === activeNav ? "active-font" : "has-text-warning-dark"}
+                      onClick={() => handleNavClick(item)}
+                    >
+                      <span className="icon is-small has-text-grey-dark">
+                        <i className={`${iconClass(item)} is-size-6`}></i>
+                      </span>
+                      {item}
+                    </Link>
+                  </li>
+                ))}							
+              </ul>
+            </div>
+          </div>
+          <div className="container has-background-white">
+            <Routes>
+              <Route path="/menu" element={<Menu/>} />
+              <Route path="/contact" element={<Contact/>} />
+              <Route path="/" element={<Home/>} />
+            </Routes>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
