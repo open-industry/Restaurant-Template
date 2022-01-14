dda05bdb4f8deed621faa24fb76b480eae6d7165
anon-legion <g.sync.mobile1@gmail.com>
2022-01-14 10:38:05 +0800

    reduce bottom padding of header

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <>
       <header className="hero has-text-centered has-background-danger-dark">
-        <div className="hero-body">
+        <div className="hero-body pb-0">
           <h1 className="title is-1 has-text-warning">McDnld's Rstrnt</h1>
           <h2 className="subtitle is-3 has-text-white-ter">Fine Dilatory Cuisine</h2>
         </div>
