9e51839ae2f9341e12b3a8c87baca04f288b30cb
anon-legion <g.sync.mobile1@gmail.com>
2022-03-08 21:42:04 +0800

    add cart icon and style rules for cart and tenative logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 // BrowserRouter in index.js
 import { Route, Routes } from 'react-router-dom';
+import { TiShoppingCart } from 'react-icons/ti';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
 import Contact from './pages/Contact';
@@ -9,9 +10,19 @@ import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
 import { LocationProvider } from './components/navContext';
 import './App.css';
-// import 'bulma/css/bulma.min.css';
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const addItem = (item, qty) => {
+    const itemInCart = cart.find((cartItem) => cartItem.id === item.id);
+    if (itemInCart) {
+      itemInCart.qty += qty;
+    } else {
+      setCart((prevState) => [...prevState, { ...item, qty }]);
+    }
+  };
+
   return (
     <>
       <header className="hero has-text-centered has-background-danger-dark">
@@ -22,12 +33,17 @@ function App() {
       </header>
       <main className="section has-background-danger-dark">
         <LocationProvider>
-          <Sidebar />
+          <div className="container is-flex is-align-items-center is-justify-right-desktop" style={{ position: 'initial' }}>
+            <Sidebar />
+            <button className="icon is-medium" type="button">
+              <TiShoppingCart color="#f5f5f5" size="29px" />
+            </button>
+          </div>
           <div className="container">
             <div className="box">
               <HorizontalNav />
               <Routes>
-                <Route path="/menu" element={<Menu />} />
+                <Route path="/menu" element={<Menu cart={cart} addToCart={addItem} />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/" element={<Home />} />
               </Routes>
